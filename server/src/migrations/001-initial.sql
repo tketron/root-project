7a63097a481822ff8b590bdb5c4b07373eb07d0d
Tyler Ketron <tketron@gmail.com>
2025-02-09 14:22:51 -0800

    add migration to seed database

--- server/src/migrations/001-initial.sql
+++ server/src/migrations/001-initial.sql
@@ -1,17 +1,18 @@
-
 -- Up
 CREATE TABLE Suggestions (
-    suggestion_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    -- In production would use AUTOINCREMENT instead of ROWID
+    suggestion_id INTEGER PRIMARY KEY DEFAULT ROWID NOT NULL,
     content TEXT NOT NULL,
     author TEXT NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
 CREATE TABLE Comments (
-    comment_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    -- In production would use AUTOINCREMENT instead of ROWID
+    comment_id INTEGER PRIMARY KEY DEFAULT ROWID NOT NULL,
     content TEXT NOT NULL,
     author TEXT NOT NULL,
-    suggestion_id INTEGER NOT NULL REFERENCES Suggestions(suggestion_id),
+    suggestion_id INTEGER NOT NULL REFERENCES Suggestions(suggestion_id) ON DELETE CASCADE,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
